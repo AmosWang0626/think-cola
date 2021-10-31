ccb3656b6ea81835230b5016df3d84bc7e72514a
AMOS <1833063210@qq.com>
2021-10-31 12:18:30 +0800

    refactor: Execute initialization sql

---
+++ think-infrastructure/src/main/resources/schema-h2.sql
@@ -0,0 +1,29 @@
+-- ----------------------------
+-- Table structure for org_user
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `org_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `info_id` varchar(255) DEFAULT NULL,
+  `delete_flag` bit(1) NOT NULL,
+  `creator` varchar(255) DEFAULT NULL,
+  `gmt_create` datetime DEFAULT NULL,
+  `modifier` varchar(255) DEFAULT NULL,
+  `gmt_modify` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ----------------------------
+-- Table structure for org_user_info
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `org_user_info` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `phone_no` varchar(255) DEFAULT NULL,
+  `gender` int(11) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
